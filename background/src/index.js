a89946cc2092f28bbfe633d4148cad0faab2ffac
Andrew Elmore <andrewelmore93@gmail.com>
2020-12-23 01:40:47 -0500

    add webpack for background

---
+++ background/src/index.js
@@ -0,0 +1,19 @@
+console.log('from background')
+
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        console.log(current_tab_info.url)
+        if (/^https:\/\/www\.linkedin\.com*/.test(current_tab_info.url)) {
+            chrome.tabs.executeScript(null, { file: './foreground.js' }, () => { console.log('injected') })
+        } else {
+            console.log('not')
+        }
+    })
+})
+
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.message === 'linkedin parse complete')
+        chrome.storage.local.get('linkedinPayload', storage => {
+            console.log(storage.linkedinPayload)
+        })
+})
